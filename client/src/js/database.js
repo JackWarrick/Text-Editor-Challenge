c5d6f31ab658b1d6852f0a5bea022520527a53c5
Jack Warrick <jackwarrick@Jacks-MacBook-Pro.local>
2022-11-23 18:23:06 -0600

    install.js and get method of database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,6 +17,23 @@ const initdb = async () =>
 export const putDb = async (content) => console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('get from database');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+
+  console.log('result.value', result);
+  return result;
+
+
+}
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,38 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+
+//DONE - MINI PROJECT AID
+
+window.addEventListener('beforeinstallprompt', (event) => {
+
+    window.deferredPrompt = event;
+
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+
+//DONE - MINI PROJECT AID
+
+butInstall.addEventListener('click', async () => {
+
+const promptEvent = window.deferredPrompt;
+
+if (!promptEvent){
+    return;
+}
+
+promptEvent.prompt();
+
+window.deferredPrompt = null('hidden', true);
+
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+
+//DONE - MINI PROJECT AID
+window.addEventListener('appinstalled', (event) => {
+
+    window.deferredPrompt = null
+});
