488d12aa55313f64340a46a9d6086ebf5e5e747d
Jack Warrick <jackwarrick@Jacks-MacBook-Pro.local>
2022-11-26 15:57:05 -0600

    just need to figure out the install button

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,7 +2,6 @@ import { Workbox } from 'workbox-window';
 import Editor from './editor';
 import './database';
 import '../css/style.css';
-import { header } from './header';
 
 const main = document.querySelector('#main');
 main.innerHTML = '';
@@ -27,8 +26,8 @@ if (typeof editor === 'undefined') {
 
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
-  // register workbox service worker
-  const workboxSW = new Workbox('./src-sw.js');
+  // register workbox service worker - changed from ./src-sw.js
+  const workboxSW = new Workbox('/src-sw.js');
   workboxSW.register();
 } else {
   console.error('Service workers are not supported in this browser.');
